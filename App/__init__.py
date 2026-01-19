d01c5e4cf655cae6c05518d4f7129477381f95b7
theotrc <theotricot12@gmail.com>
2026-01-19 16:10:05 +0100

    dowload model

--- App/__init__.py
+++ App/__init__.py
@@ -2,9 +2,16 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from App.utils import download_model
+
 
 
 app = Flask(__name__)
+
+model_path = download_model()
+
+print("Model downloaded to:", model_path)
+
 
 # During testing we avoid importing heavy TensorFlow packages and loading
 # the model at import time. CI/tests should set `TESTING=1` to use a stub.
@@ -20,7 +27,9 @@ else:
 	import tensorflow_text
 	import tf_keras
 
-	bert_model = tf_keras.models.load_model(os.path.join('App', 'models', 'saved_model2'))
+	
+	print("Loading model from:", model_path)
+	bert_model = tf_keras.models.load_model(model_path)
 
 from App import views
 
--- App/utils.py
+++ App/utils.py
@@ -5,6 +5,12 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle
+import os
+import json
+from google.cloud import storage
+from dotenv import load_dotenv
+
+
 
 
 
@@ -29,6 +35,43 @@ class TextTransformer:
         tokens = [t for t in s.split() if t not in self.stop]
         tokens = [self.lemmatizer.lemmatize(t) for t in tokens]
         return " ".join(tokens)
+
+
+
+def download_model():
+    load_dotenv(override=True)
+    
+    creds = json.loads(os.environ["GCP_SECRET"])
+    creds_path = "/tmp/gcp_key.json"
+    
+    with open(creds_path, "w") as f:
+        json.dump(creds, f)
+        
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = creds_path
+
+    model_folder = os.environ["MODEL_FOLDER"]
+    client = storage.Client()
+    bucket = client.bucket(os.environ["GCS_BUCKET"])
+    
+    print("BUCKET NAME:", bucket)
+    bloblist = bucket.list_blobs()
+
+    for blob in bloblist:
+        print("BLOB NAME:", blob.name)
+
+        local_path = os.path.join("/tmp", blob.name)
+        
+        # créer les dossiers parents
+        os.makedirs(os.path.dirname(local_path), exist_ok=True)
+
+        blob.download_to_filename(local_path)
+        
+    return os.path.join("/tmp", model_folder)
+    
+    
+    
+    
+        
     
     
     
